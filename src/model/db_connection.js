36aec3939b468101939aa6938a4c377945f81ee8
sallamtanna <sallamtanna2015@hotmail.com>
2018-06-08 15:40:10 +0300

    new commit for heroku

--- src/model/db_connection.js
+++ src/model/db_connection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-if (!process.env.DB_URL) throw new Error('DB_URL is not set!');
-const url = process.env.DB_URL;
+if (!process.env.DATABASE_URL) throw new Error('DB_URL is not set!');
+const url = process.env.DATABASE_URL;
 
 module.exports = new Pool({ connectionString: url, ssl: true });
